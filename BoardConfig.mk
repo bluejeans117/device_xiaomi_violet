c4906f17dfd19033cd349d326460f13c28d1457a
TheScarastic <warabhishek@gmail.com>
2019-01-30 18:16:42 +0530

    violet: Switch to more generic dt2w node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,7 @@ TARGET_USES_MKE2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event2"
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
