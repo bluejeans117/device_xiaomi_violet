9e5286d59b311223c05128c45e4fa08a10a0578a
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    violet: Convert remaining configstore settings to props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,6 +126,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.protected_contents=true
+
 # WFD
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
