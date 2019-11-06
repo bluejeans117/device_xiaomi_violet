91adcfc8bec06f418f5528429c483c389cda40a2
bluejeans117 <vishnurajesh45@gmail.com>
2019-11-06 10:30:07 +0000

    Revert "Revert "Revert "Revert "violet: Introduce Safailnet""""
    K
    
    This reverts commit dad8626da60ce9006fd908a28d37a0fcbae5d15d.

--- device.mk
+++ device.mk
@@ -342,7 +342,8 @@ PRODUCT_PACKAGES += \
     init.qti.qseecomd.sh \
     init.target.rc \
     ueventd.qcom.rc \
-    fstab.qcom
+    fstab.qcom \
+    init.safailnet.rc
 
 # RCS
 PRODUCT_PACKAGES += \
