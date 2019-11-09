e6576d360f2f57ceba95761438b101757316e791
bluejeans117 <vishnurajesh45@gmail.com>
2019-11-09 17:39:37 +0000

    violet: power: switch to libperfmgr for powerhint

--- device.mk
+++ device.mk
@@ -303,11 +303,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.violet
+     android.hardware.power@1.2-service.violet-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    device/xiaomi/violet/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
