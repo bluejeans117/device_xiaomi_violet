16cea39beffb935adbeb72244de553edecfe7ab9
Udit Karode <udit.karode@gmail.com>
2019-11-28 09:09:10 +0000

    violet: introduce Pixel Live Wallpapers
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- device.mk
+++ device.mk
@@ -408,6 +408,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Wallpapers
+PRODUCT_PACKAGES += \
+    PixelLiveWallpaper
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
