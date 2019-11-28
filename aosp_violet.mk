b08614d2674fc2a5c0295c08c7165cb100b8d43a
Udit Karode <udit.karode@gmail.com>
2019-11-28 08:59:21 +0000

    treewide: generalise

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -13,11 +13,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common Bootleggers stuff.
-$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+# Inherit some common rom specific stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bootleg_violet
+PRODUCT_NAME := aosp_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
@@ -26,6 +26,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet" \
     TARGET_DEVICE="violet" \
-    DEVICE_MAINTAINERS="merothh"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
